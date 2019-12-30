df7524b63eac32d848eb7bed0307958ac7565483
Daniel Lopez <d.lopezm@edu.uah.es>
2019-12-30 16:10:13 +0100

    Miniproyecto actualizacion 5

--- Laboratorio/Miniproyecto/ArchivosMiniProyectov2/Matlab/Ejecutar.m
+++ Laboratorio/Miniproyecto/ArchivosMiniProyectov2/Matlab/Ejecutar.m
@@ -5,5 +5,5 @@ rosshutdown
 ROS_MASTER_IP = '192.168.1.48'
 ROS_IP = '192.168.56.1'
 
-%rosinit(['http://',ROS_MASTER_IP,':11311'],'NodeHost',ROS_IP)
+rosinit(['http://',ROS_MASTER_IP,':11311'],'NodeHost',ROS_IP)
 sim("test_controler_generic.slx")
