997df9893631d8ac5dd5ce74c2d642c25b0ca947
Alexcp35 <alexcp35525@gmail.com>
2020-01-04 14:06:51 +0100

    SUGENO empezado

--- Laboratorio/Miniproyecto/ArchivosMiniProyectov2/Matlab/Ejecutar.m
+++ Laboratorio/Miniproyecto/ArchivosMiniProyectov2/Matlab/Ejecutar.m
@@ -2,8 +2,8 @@ clear all;
 close all;
 rosshutdown
 
-ROS_MASTER_IP = '192.168.1.48'
-ROS_IP = '192.168.56.1'
+ROS_MASTER_IP = '192.168.8.109'
+ROS_IP = '192.168.8.109'
 
 rosinit(['http://',ROS_MASTER_IP,':11311'],'NodeHost',ROS_IP)
-sim("test_controler_generic.slx")
+sim("test_controler_sugeno.slx")
---
+++ Laboratorio/Miniproyecto/ArchivosMiniProyectov2/Matlab/EjecutarSugeno.m
@@ -0,0 +1,6 @@
+load ("datos_entrenamiento.mat")
+train_angular = training(:,[1,3,4,12])
+indices = round(linspace(1,size(training,1),1500))
+train_angular = train_angular(indices,:)
+train_angular(isinf(train_angular)) = 5.0
+train_angular = double(train_angular)
