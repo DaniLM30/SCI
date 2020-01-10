2769cb0fdf7923db464022df26cc71a380d330a9
Daniel Lopez <d.lopezm@edu.uah.es>
2020-01-10 13:53:17 +0100

    Memoria Mamdani hecho

--- Laboratorio/Miniproyecto/ArchivosMiniProyectov2/Matlab/Ej_Mamdani.m
+++ Laboratorio/Miniproyecto/ArchivosMiniProyectov2/Matlab/Ej_Mamdani.m
@@ -2,8 +2,4 @@ clear all;
 close all;
 rosshutdown
 
-ROS_MASTER_IP = '192.168.1.26'
-ROS_IP = '192.168.56.1'
-
-%rosinit(['http://',ROS_MASTER_IP,':11311'],'NodeHost',ROS_IP)
 sim("cont_mamdani.slx")
